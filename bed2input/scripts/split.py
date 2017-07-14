c5f98a8bf1ec7151d530fb6377a7bfa962d86240
Yanyu Liang <yanyul@andrew.cmu.edu>
2017-07-14 17:01:14 -0500

    debugged

--- bed2input/scripts/split.py
+++ bed2input/scripts/split.py
@@ -8,22 +8,23 @@ parser.add_argument('--outdir')
 args = parser.parse_args()
 
 import sys
-if '../scripts/' in sys.path:
+if '../scripts/' not in sys.path:
     sys.path.insert(0, '../scripts/')
 import my_python
 
-size = int(my_python.get_number_of_lines(args.input) / nchunk)
+size = int(my_python.get_number_of_lines(args.input) / args.nchunk)
 
 line_counter = 0
 file_counter = 0
-with lines as open(args.input, 'r'):
+with open(args.input, 'r') as lines:
     o_name = '{outdir}/chunk_{i}.raw'.format(outdir=args.outdir, i=file_counter)
     file_counter += 1
     o = open(o_name, 'w')
     for l in lines:
         o.write(l)
+        # print(l)
         line_counter += 1
-        if line_counter >= size and line_counter < args.nchunk:
+        if line_counter >= size and file_counter < args.nchunk:
             o_name = '{outdir}/chunk_{i}.raw'.format(outdir=args.outdir, i=file_counter)
             file_counter += 1
             o.close()
--- scripts/my_python.py
+++ scripts/my_python.py
@@ -1,1 +1,7 @@
-def 
+import subprocess
+
+def get_number_of_lines(filename):
+    cmd = 'cat ' + filename + ' | wc -l'
+    result = subprocess.check_output(cmd, shell=True)
+    nrow = int(result.decode().split('\n')[0])
+    return nrow 
