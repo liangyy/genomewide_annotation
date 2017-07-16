6d36a57c442a249dd33f660ff5a3928bec0000c1
Yanyu Liang <yanyul@andrew.cmu.edu>
2017-07-16 14:55:00 -0500

    added new scripts

--- scripts/my_python.py
+++ scripts/my_python.py
@@ -1,7 +1,14 @@
 import subprocess
+import h5py
 
 def get_number_of_lines(filename):
     cmd = 'cat ' + filename + ' | wc -l'
     result = subprocess.check_output(cmd, shell=True)
     nrow = int(result.decode().split('\n')[0])
-    return nrow 
+    return nrow
+
+def read_hdf5_by_name(filename, name):
+    f_scores = h5py.File(filename, 'r')
+    scores = f_scores[name][()]
+    f_scores.close()
+    return scores
