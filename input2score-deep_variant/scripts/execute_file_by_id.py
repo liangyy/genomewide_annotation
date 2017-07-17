0f91165d7e7eb64c1002bc9fa2620076c4bb04fc
Yanyu Liang <yanyul@andrew.cmu.edu>
2017-07-17 12:49:54 -0500

    added script to execute file by id

---
+++ input2score-deep_variant/scripts/execute_file_by_id.py
@@ -0,0 +1,21 @@
+import argparse
+parser = argparse.ArgumentParser(prog='execute_file_by_id.py', description='''
+    Execute file by the given ID list.
+''')
+parser.add_argument('--prefix', help='''
+    Prefix of filename (exclude dirname)
+''')
+parser.add_argument('--suffix')
+parser.add_argument('--execute')
+parser.add_argument('-id_list', help = 'Separate by ,')
+args = parser.parse_args()
+
+import os
+
+ids = [ i for i in args.id_list.split(',') ]
+
+for idx in ids:
+	filename = '{prefix}{idx}{suffix}'.format(prefix=args.prefix, suffix=args.suffix, idx=idx)
+	cmd = '{exe} {file}'.format(exe=args.execute, file=filename)
+	os.system(cmd)
+	
