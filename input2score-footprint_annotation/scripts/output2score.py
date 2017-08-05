a6d52d326564ccaf693ed5c60cb2b2673e028306
Yanyu Liang <yanyul@andrew.cmu.edu>
2017-08-05 12:00:59 -0500

    debugged

--- input2score-footprint_annotation/scripts/output2score.py
+++ input2score-footprint_annotation/scripts/output2score.py
@@ -13,20 +13,22 @@ if '../scripts/' not in sys.path:
     sys.path.insert(0, '../scripts/')
 import my_python
 import pandas as pd
+import numpy as np
 
-input_snp = pd.read_table(args.input_snp, header=0, usecols=[0, 3, 4, 5, 6, 7, 8, 9])
-input_score = pd.input_score(args.input_score, header=None, names=['Chr', 'Start', 'End', 'Allele1', 'Allele2', 'ID', 'Motif'], usecols=[0, 1, 2, 3, 4, 5, 9])
-feature_a1 = input_snp['LogRatioPrior.Ref']
-feature_a2 = input_snp['LogRatioPrior.Alt']
+input_score = pd.read_table(args.input_score, header=0, usecols=[0, 3, 8, 9], dtype={'SNP.ID': np.int32, 'Motif.ID': str, 'LogRatioPrior.Ref': np.float64, 'LogRatioPrior.Alt': np.float64})
+input_snp = pd.read_table(args.input_snp, header=None, compression='gzip', converters={'Chr': str, 'Start': np.int32, 'End': np.int32, 'Allele1': str, 'Allele2': str, 'ID': np.int32, 'Motif': str}, usecols=[0, 1, 2, 3, 4, 5, 9], names=['Chr', 'Start', 'End', 'Allele1', 'Allele2', 'ID', 'Motif'])
+feature_a1 = input_score['LogRatioPrior.Ref']
+feature_a2 = input_score['LogRatioPrior.Alt']
 feature = [ ','.join([str(a), str(b)]) for (a, b) in zip(feature_a1, feature_a2) ]
-input_score['LogRatioPrior'] = feature
-combined = pd.concat([input_score, input_snp.ix[:, [-4, -2, -1]], axis=1, ignore_index=True)
-grouped = combined.groupby(['Chr', 'Start', 'End', 'Motif'], sort=False)
+input_snp['LogRatioPrior'] = feature
+input_snp['Allele'] = input_snp.apply(lambda x: ','.join([x['Allele1'], x['Allele2']]), axis=1)
+
+grouped = input_snp.groupby(['Chr', 'Start', 'End', 'Motif'], sort=False)
 feature_agg = grouped.agg({
     'Allele' : lambda x: ','.join(x),
     'LogRatioPrior' : lambda x: ','.join(x)
 })
-feature_agg.to_csv('{prefix}.{feature}.score.gz'.format(prefix=args.out_prefix, feature=label_name),
+feature_agg.to_csv(args.output,
     sep='\t',
     compression='gzip',
     header=False)
