3aa127f0a0205ff5108aa0b1ff7acf4840133ab4
Yanyu Liang <yanyul@gmail.uchicago.edu>
2017-11-29 22:43:35 -0600

    fixed bad bug

--- input2score-footprint_annotation/scripts/output2score.py
+++ input2score-footprint_annotation/scripts/output2score.py
@@ -15,7 +15,15 @@ import my_python
 import pandas as pd
 import numpy as np
 
-input_score = pd.read_table(args.input_score, compression='gzip', header=0, usecols=[0, 3, 8, 9], dtype={'SNP.ID': np.int32, 'Motif.ID': str, 'LogRatioPrior.Ref': np.float64, 'LogRatioPrior.Alt': np.float64})
+input_score = pd.read_table(args.input_score, compression='gzip', header=0,
+    usecols=[0, 3, 8, 9],
+    dtype={'SNP.ID': np.int32,
+        'Motif.ID': str,
+        'LogRatioPrior.Ref': np.float64,
+        'LogRatioPrior.Alt': np.float64})
+        # 'Motif.Chr': str})
+        # 'Motif.Start', np.int32,
+        # 'Motif.End': np.int32})
 
 if input_score.shape[0] == 0:
     temp = pd.DataFrame([])
@@ -24,14 +32,27 @@ if input_score.shape[0] == 0:
         compression='gzip',
         header=False)
     sys.exit()
-input_snp = pd.read_table(args.input_snp, header=None, compression='gzip', converters={'Chr': str, 'Start': np.int32, 'End': np.int32, 'Allele1': str, 'Allele2': str, 'ID': np.int32, 'Motif': str}, usecols=[0, 1, 2, 3, 4, 5, 9], names=['Chr', 'Start', 'End', 'Allele1', 'Allele2', 'ID', 'Motif'])
+input_snp = pd.read_table(args.input_snp, header=None, compression='gzip',
+    converters={'Chr': str,
+        'Start': np.int32,
+        'End': np.int32,
+        'Allele1': str,
+        'Allele2': str,
+        'ID': np.int32,
+        'Motif': str,
+        'Motif.Start': np.int32,
+        'Motif.End': np.int32},
+    usecols=[0, 1, 2, 3, 4, 5, 9],
+    names=['Chr', 'Start', 'End', 'Allele1', 'Allele2', 'ID', 'Motif', 'Motif.Start', 'Motif.End'])
+input_snp['Motif.Start'] = input_snp['Motif.Start'] - 1
+input_snp['Motif.End'] = input_snp['Motif.End'] - 1
 feature_a1 = input_score['LogRatioPrior.Ref']
 feature_a2 = input_score['LogRatioPrior.Alt']
 feature = [ ','.join([str(a), str(b)]) for (a, b) in zip(feature_a1, feature_a2) ]
 input_snp['LogRatioPrior'] = feature
 input_snp['Allele'] = input_snp.apply(lambda x: ','.join([x['Allele1'], x['Allele2']]), axis=1)
 
-grouped = input_snp.groupby(['Chr', 'Start', 'End', 'Motif'], sort=False)
+grouped = input_snp.groupby(['Chr', 'Start', 'End', 'Motif', 'Motif.Start', 'Motif.End'], sort=False)
 feature_agg = grouped.agg({
     'Allele' : lambda x: ','.join(x),
     'LogRatioPrior' : lambda x: ','.join(x)
