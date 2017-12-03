f4c5430c653c1a882450d53f3f40eff2f82b2ee5
Yanyu Liang <yanyul@gmail.uchicago.edu>
2017-12-03 14:14:18 -0600

    added new rules to correct score

---
+++ input2score-footprint_annotation/scripts/correct_score.R
@@ -0,0 +1,54 @@
+library(optparse)
+
+option_list <- list(
+  make_option(c("-s", "--score"), type="character", default=NULL,
+              help="score to correct",
+              metavar="character"),
+  make_option(c("-r", "--reference"), type="character", default=NULL,
+              help="reference model in RDS",
+              metavar="character"),
+  make_option(c("-o", "--output"), type="character", default=NULL,
+              help="output file name",
+              metavar="character")
+)
+
+opt_parser <- OptionParser(option_list=option_list)
+opt <- parse_args(opt_parser)
+
+library(stringr)
+library(dplyr)
+
+# score to be corrected
+data <- read.table(opt$score, sep = '\t', header = F)
+
+v11.good <- is.na(str_match(data$V7, '[ATGC]'))
+v11.strategy1 <- str_match(data$V7, '(.+),(.+),(.+),(.+)')
+v12.good <- is.na(str_match(data$V8, '[ATGC]'))
+v12.strategy1 <- str_match(data$V8, '(.+),(.+),(.+),(.+)')
+strategy1 <- v11.strategy1
+strategy1[v12.good, ] <- v12.strategy1[v12.good, ]
+# strategy1 <- str_match(data$V12, '(.+),.+,.+,.+')
+strategy1 <- strategy1[, 2 : 5]
+class(strategy1) <- 'numeric'
+corrected <- matrix(0, nrow = nrow(strategy1), ncol = 4)
+
+# load reference model
+loadRDS(opt$reference)
+for (i in names(model)) {
+  model.sub <- model[[i]]
+  ind.sub <- data$V5 == i
+  strategy1.sub <- strategy1[ind.sub, ]
+  corrected.sub <- matrix(0, nrow = nrow(model.sub), ncol = 4)
+  for (j = 1 : 4){
+    corrected.sub[, j] <- predict(model.sub, data.frame(strategy1 = strategy1.sub[, j]))
+  }
+  corrected[ind.sub, ] <- corrected.sub
+}
+
+corrected_str <- apply(corrected, 1, function(x) {paste(x, collapse = ',')})
+data[v11.good, 'V7'] <- corrected_str[v11.good]
+data[v12.good, 'V8'] <- corrected_str[v12.good]
+
+gz <- gzfile(opt$output, 'w')
+write.table(data, gz, sep = '\t', quote = F, col.names = F, row.names = F)
+close(gz)
---
+++ input2score-footprint_annotation/scripts/generate_reference_model.R
@@ -0,0 +1,43 @@
+library(optparse)
+
+option_list <- list(
+  make_option(c("-r", "--reference"), type="character", default=NULL,
+              help="reference scores",
+              metavar="character"),
+  make_option(c("-o", "--output"), type="character", default=NULL,
+              help="output file name",
+              metavar="character")
+)
+
+opt_parser <- OptionParser(option_list=option_list)
+opt <- parse_args(opt_parser)
+
+library(stringr)
+library(dplyr)
+
+data <- read.table('{input}', sep = '\\t', header = F)
+
+v11.good <- is.na(str_match(data$V14, '[ATGC]'))
+v11.strategy1 <- str_match(data$V14, '(.+),.+,.+,.+')
+v12.good <- is.na(str_match(data$V15, '[ATGC]'))
+v12.strategy1 <- str_match(data$V15, '(.+),.+,.+,.+')
+strategy1 <- v11.strategy1
+strategy1[v12.good, ] <- v12.strategy1[v12.good, ]
+# strategy1 <- str_match(data$V12, '(.+),.+,.+,.+')
+strategy1 <- strategy1[, 2]
+class(strategy1) <- 'numeric'
+data <- data.frame(strategy1 = strategy1, centisnp = data$V7, motif = data$V4, position = data$V2, strand = data$V6)
+data <- unique(data)
+motifs <- unique(data$motif)
+
+data <- data.frame(strategy1 = strategy1, centisnp = data$V7, motif = data$V4, position = data$V2, strand = data$V6)
+data <- unique(data)
+motifs <- unique(data$motif)
+
+model <- list()
+for (i in motifs) {
+    data.sub <- data[data$motif == i, ]
+    model.sub <- lm(centisnp ~ strategy1, data = data.sub)
+    model[[i]] <- model.sub
+}
+saveRDS(object = model, file = opt$output)
